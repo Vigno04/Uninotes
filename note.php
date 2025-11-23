b2d07e8f2dac9214bab33ba2d223dabe32faa61a
Vigno04 <davivigna2004@gmail.com>
2025-11-23 17:23:38 +0100

    note renderer now can view files and images

--- note.php
+++ note.php
@@ -1,6 +1,7 @@
 <?php
 require_once("bootstrap.php");
 require_once 'db/NoteModel.php';
+require_once 'db/db.php';
 require_once 'vendor/parsedown/Parsedown.php';
 
 $id = $_GET['id'] ?? null;
@@ -9,8 +10,69 @@ if ($id !== null && is_numeric($id)) {
     $model = new NoteModel();
     $note = $model->getById((int)$id);
     if ($note) {
+        $pdo = Database::getInstance();
+        
+        // Query files for this note
+        $stmt = $pdo->prepare("SELECT id, filename, mime_type FROM file WHERE note_id = ?");
+        $stmt->execute([$note['id']]);
+        $files = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        
+        // Build filename => file details map
+        $fileMap = [];
+        foreach ($files as $file) {
+            $fileMap[$file['filename']] = [
+                'id' => $file['id'],
+                'mime' => $file['mime_type']
+            ];
+        }
+        
+        // Render Markdown to HTML
         $Parsedown = new Parsedown();
         $htmlContent = $Parsedown->text($note['content']);
+        
+        // Replace filenames with secure URLs using DOM manipulation
+        if (!empty($fileMap)) {
+            $dom = new DOMDocument();
+            libxml_use_internal_errors(true); // Suppress warnings
+            $dom->loadHTML('<?xml encoding="UTF-8">' . $htmlContent, LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD);
+            libxml_clear_errors();
+            
+            // Handle images
+            $images = $dom->getElementsByTagName('img');
+            foreach ($images as $img) {
+                $src = $img->getAttribute('src');
+                if (isset($fileMap[$src])) {
+                    $img->setAttribute('src', 'file.php?id=' . $fileMap[$src]['id']);
+                }
+            }
+            
+            // Handle links (for downloadable files)
+            $links = $dom->getElementsByTagName('a');
+            foreach ($links as $link) {
+                $href = $link->getAttribute('href');
+                if (isset($fileMap[$href])) {
+                    $link->setAttribute('href', 'file.php?id=' . $fileMap[$href]['id']);
+                    // Add download attribute for non-images
+                    if (!str_starts_with($fileMap[$href]['mime'] ?? '', 'image/')) {
+                        $link->setAttribute('download', '');
+                    }
+                }
+            }
+            
+            $htmlContent = $dom->saveHTML();
+        }
+        
+        // Add file list at the end if there are files
+        if (!empty($files)) {
+            $fileList = '<div class="attached-files mt-4"><h3>Attached Files</h3><ul>';
+            foreach ($files as $file) {
+                $url = 'file.php?id=' . $file['id'];
+                $fileList .= '<li><a href="' . htmlspecialchars($url) . '" download>' . htmlspecialchars($file['filename']) . '</a></li>';
+            }
+            $fileList .= '</ul></div>';
+            $htmlContent .= $fileList;
+        }
+        
         $templateParams = ["title" => "Nota"];
         include("template/note.php");
     } else {
