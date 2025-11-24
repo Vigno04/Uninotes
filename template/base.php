002b359ac5f71f7dfc9b91466a9bd9689897cc1b
Veenya <veramurgia02@gmail.com>
2025-11-24 09:16:51 +0100

    WIP:save current changes

--- template/base.php
+++ template/base.php
@@ -13,7 +13,6 @@ $profilePage = 'account';
     <title><?php echo htmlspecialchars($templateParams["title"] ?? "UniNotes"); ?></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-    <link rel="stylesheet" href="css/style.css">
 </head>
 <body class="bg-light">
 
